757ca4482b94398eec48a93d730adf5b4d126e86
Kanjuu <hugo.batt.pro@gmail.com>
2023-11-23 16:57:07 +0100

    cleaned imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import {ThemeProvider, createTheme} from '@mui/material/styles'
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import {Home} from "./pages/home/home";
-import {ContactMe} from "./pages/contact-me/contact-me";
-import {Portfolio} from "./pages/portfolio/portfolio";
+import {BrowserRouter} from "react-router-dom";
 import AnimatedRoutes from "./components/routing/Animated-routes";
 
 const theme = createTheme({
